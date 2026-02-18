94c682aaf21595628b18f9d0d3b0e73cfbdc68f3
KJohanAndersson <johan.andersson0355@stud.hkr.se>
2026-02-18 12:29:33 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -499,7 +499,7 @@ export default function App() {
             </div>
 
             {/* HP Text */}
-            <div style={{ textAlign: 'center', marginBottom: 10 }}>
+            <div style={{ textAlign: 'center', marginTop: 30, marginBottom: 10 }}>
               <p>Head HP: {boss.head}</p>
               <p>Body HP: {boss.body}</p>
               <p>Shield HP: {boss.shield}</p>
